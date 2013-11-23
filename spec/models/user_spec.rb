38f42b4fd47bef15774ccca60e845c627eb79e5c
Susana Ramallo <ramallo.susana@gmail.com>
2013-11-23 16:59:13 -0600

    Finish sign in

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -16,6 +16,7 @@ describe User do
   it { should respond_to(:password_digest) }
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
+  it { should respond_to(:remember_token) }
   it { should respond_to(:authenticate) }
 
   it { should be_valid }    # verifying that the subject (@user) is initially valid
@@ -28,6 +29,12 @@ describe User do
   describe "when name is too long" do
     before { @user.name = "a" * 51 }
     it { should_not be_valid }
+  end
+
+ describe "remember token" do
+    before { @user.save }
+    its(:remember_token) { should_not be_blank }
+    # its is like it but applies the subsequent test to the given attribute rather than the subject of the test
   end
 
   describe "when email is not present" do
